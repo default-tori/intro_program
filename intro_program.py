573899b002c3d524a0c565fe36415bc57d7a804e
default-tori <hikikitori7@gmail.com>
2025-10-25 23:09:55 +0100

    My first Python introduction program

---
+++ intro_program.py
@@ -0,0 +1,15 @@
+print("Hello User")
+Hello User
+name= "NiNeD_EyE"
+print("I am", name)
+I am NiNeD_EyE
+name= input("What is your name?") # Ask User for their name
+What is your name?Tori
+print("Hello", name, "!") # Greet them
+Hello Tori !
+while True:
+     name= input("What is your name?") # Ask User for their name
+     print("Hello", name, "!") # Greet them
+     repeat= input("Do you want to continue? (yes/no): ")
+     if repeat.lower() != "yes":
+         break
