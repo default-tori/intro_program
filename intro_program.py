2c5c01c28dba3a6e666784a236eb0cadf91eb036
default-tori <hikikitori7@gmail.com>
2026-01-21 15:46:19 +0100

    Update intro_program.py

--- intro_program.py
+++ intro_program.py
@@ -1,9 +1,6 @@
 print("Hello User")
 name= "NiNeD_EyE"
 print("I am", name)
-name= input("What is your name?") # Ask User for their name
-What is your name?
-print("Hello", name, "!") # Greet them
 while True:
      name= input("What is your name?") # Ask User for their name
      print("Hello", name, "!") # Greet them
